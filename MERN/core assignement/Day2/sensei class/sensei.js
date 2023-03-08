d18a9a74111ac8b68617e3f22091a5a344a7206b
MehdiOmrani <86834091+MehdiTony@users.noreply.github.com>
2023-03-08 07:58:29 +0100

    Add files via upload

---
+++ MERN/core assignement/Day2/sensei class/sensei.js
@@ -0,0 +1,18 @@
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom(); 
+superSensei.showStats(); 
